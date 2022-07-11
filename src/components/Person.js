e5877d15385d4d6b39d71f4fe3a3ffc8248d32bd
Ifo Ikede <ifoitech@gmail.com>
2022-07-11 02:17:12 -0300

    add bootstrap styling to person form elements

--- src/components/Person.js
+++ src/components/Person.js
@@ -6,11 +6,12 @@ import React from 'react';
 function Person(props) {
 
 
-    return ( <div>    <h3> Personal Information </h3>
+    return ( <div className="form-group" > 
+          <h3> Personal Information </h3>
         <label htmlFor="firsName"  > First Name: </label> 
         <input type="text" 
                name="firstName"
-              
+               className="form-control"
                id="FirstName"
                value={props.personalInfo.FirstName}
                onChange={props.handleChange}
@@ -19,20 +20,20 @@ function Person(props) {
     <label htmlFor="LastName"    >Last Name: </label> 
         <input type="text" 
                name="LastName"
-              
+               className="form-control"
                id="LastName"
                value={props.personalInfo.LastName}
                onChange={props.handleChange}
                placeholder="Enter your Last Name"/>                
         
         <label htmlFor="email" > email: </label> 
-        <input type="email"  id="email" name="email"
+        <input type="email"  id="email" name="email" className="form-control"
             value={props.personalInfo.email}
             onChange={props.handleChange}
             placeholder="Enter email"/> 
 
-       <label htmlFor="phone" > phone: </label> 
-       <input type="tel"  id="phone" name="phone" value={props.personalInfo.phone} onChange={props.handleChange} placeholder="Enter phone"/> 
+       <label htmlFor="phone" className="col-form-label"> phone: </label> 
+       <input type="tel"  id="phone" name="phone" className="form-control" value={props.personalInfo.phone} onChange={props.handleChange} placeholder="Enter phone"/> 
   
     </div>
      );
