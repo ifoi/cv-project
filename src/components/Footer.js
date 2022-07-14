3276b1ec32565355285873b4fca3b819bf42e876
Ifo Ikede <ifoitech@gmail.com>
2022-07-13 21:59:50 -0300

    fix crash on deleting  last education/experience

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ const Footer =(props) => {
    return ( <div className="bg-dark text-white">
     
         <p>  This is the resume of {props.personalInfo.firstName}  </p>
-        <p> Worked at {props.experience[0].companyName} </p> 
-        <p> Studied at {props.education[0].schoolName} </p>
+        <p> Worked at { props.experience[0] && props.experience[0].companyName} </p> 
+        <p> Studied at {props.education[0] && props.education[0].schoolName} </p>
     </div> )
 
 
